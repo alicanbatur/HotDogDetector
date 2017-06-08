33f7a3f40250c8072a102baf29f470e374b906ae
Ali Can Batur <alican.batur@sahibinden.com>
2017-06-08 12:49:19 +0300

    Initial Commit

---
+++ hotdog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hotdog
+//
+//  Created by Ali Can Batur on 08/06/2017.
+//  Copyright © 2017 alikoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
